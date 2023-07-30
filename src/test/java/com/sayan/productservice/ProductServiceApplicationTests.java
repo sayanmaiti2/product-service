0addc9e0f4a5373731003a795d350c69146f4896
Sayan Maiti <34590014+sayanmaiti2@users.noreply.github.com>
2023-07-30 20:34:23 +0530

    feat(product-service): added integration tests for Endpoints

--- src/test/java/com/sayan/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/sayan/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,75 @@
 package com.sayan.productservice;
 
+import com.sayan.productservice.dto.ProductRequest;
+import com.sayan.productservice.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
+    @Container
+    final static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @DynamicPropertySource
+    public static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
     @Test
-    void contextLoads() {
+    public void shouldCreateProduct() throws Exception {
+
+        ProductRequest productRequest = getProductRequest();
+        String productRequestString = objectMapper.writeValueAsString(productRequest);
+        mockMvc.perform(MockMvcRequestBuilders.post("/v1/api/product")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(productRequestString))
+                .andExpect(status().isCreated());
+
+        assertEquals(1, productRepository.findAll().size());
+    }
+
+    @Test
+    public void shouldFetchAllProducts() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/v1/api/product")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        assertEquals(1, productRepository.findAll().size());
+    }
+
+    private ProductRequest getProductRequest() {
+        return ProductRequest.builder()
+                .name("iPhone 13")
+                .description("iPhone 13")
+                .price(BigDecimal.valueOf(699))
+                .build();
     }
 
 }
